38df613b398eaeb261ab9a16518b130473f332ba
dnajd <dimitar.hristijan.najdovski@gmail.com>
2023-02-01 12:37:51 +0100

    adding main module files

---
+++ src/main/kotlin/com/cythero/cityguide/cityguide/CityguideApplication.kt
@@ -0,0 +1,11 @@
+package com.cythero.cityguide.cityguide
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CityguideApplication
+
+fun main(args: Array<String>) {
+	runApplication<CityguideApplication>(*args)
+}
---
+++ src/test/kotlin/com/cythero/cityguide/cityguide/CityguideApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cythero.cityguide.cityguide
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CityguideApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
