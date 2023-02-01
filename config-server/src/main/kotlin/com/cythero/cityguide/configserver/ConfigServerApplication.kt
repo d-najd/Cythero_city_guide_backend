61fb7d87082dc84f4a2e171fc377ea03a7635347
d najd <dimitar.hristijan.najdovski@gmail.com>
2023-02-01 21:34:31 +0100

    some config

--- config-server/src/main/kotlin/com/cythero/cityguide/configserver/ConfigServerApplication.kt
+++ config-server/src/main/kotlin/com/cythero/cityguide/configserver/ConfigServerApplication.kt
@@ -1,8 +1,14 @@
 package com.cythero.cityguide.configserver
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
+import org.springframework.context.annotation.Configuration
 
+@Configuration
+@EnableAutoConfiguration
+@EnableConfigServer
 @SpringBootApplication
 class ConfigServerApplication
 
--- discovery-server/src/main/kotlin/com/cythero/cityguide/discoveryserver/DiscoveryServerApplication.kt
+++ discovery-server/src/main/kotlin/com/cythero/cityguide/discoveryserver/DiscoveryServerApplication.kt
@@ -2,8 +2,10 @@ package com.cythero.cityguide.discoveryserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class DiscoveryServerApplication
 
 fun main(args: Array<String>) {
