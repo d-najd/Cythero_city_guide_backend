b0818ce720ff32ad9f94cb18e98ca48fa25b3244
d najd <dimitar.hristijan.najdovski@gmail.com>
2023-02-04 22:25:52 +0100

    adding sample db data

---
+++ database_data.sql
@@ -0,0 +1,17 @@
+INSERT IGNORE INTO users VALUES (1, "user1");
+
+INSERT IGNORE INTO users VALUES (2, "user2");
+
+INSERT IGNORE INTO users VALUES (3, "user3");
+
+INSERT IGNORE INTO locations VALUES (1, 100.02, 50.6342871, "Macedonia - Bitola?", "");
+
+INSERT IGNORE INTO locations VALUES (2, 632.2, 1245.12, "Macedonia - ?", "");
+
+INSERT IGNORE INTO countries VALUES (1, 1, "Macedonia");
+
+INSERT IGNORE INTO countries VALUES (2, 2, "Macedonia2");
+
+INSERT IGNORE INTO cities VALUES (1, 1, 1, "Bitola");
+
+INSERT IGNORE INTO cities VALUES (2, 1, 2, "Skopje");
