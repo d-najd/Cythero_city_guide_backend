4d22824790154cd5e803689413f5d8dd0fe541c6
d najd <dimitar.hristijan.najdovski@gmail.com>
2023-02-05 14:26:43 +0100

    fixing api gateway

--- api-gateway/src/main/kotlin/com/cythero/cityguide/apigateway/ApiGatewayApplication.kt
+++ api-gateway/src/main/kotlin/com/cythero/cityguide/apigateway/ApiGatewayApplication.kt
@@ -3,6 +3,12 @@ package com.cythero.cityguide.apigateway
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.client.loadbalancer.LoadBalanced
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier
+import org.springframework.context.ConfigurableApplicationContext
+import org.springframework.context.annotation.Bean
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.reactive.function.client.WebClient
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,4 +16,24 @@ class ApiGatewayApplication
 
 fun main(args: Array<String>) {
 	runApplication<ApiGatewayApplication>(*args)
+}
+
+@Bean
+@LoadBalanced
+fun loadBalancedRestTemplate(): RestTemplate {
+	return RestTemplate()
+}
+
+@Bean
+@LoadBalanced
+fun loadBalancedWebClientBuilder(): WebClient.Builder {
+	return WebClient.builder()
+}
+
+@Bean
+fun instanceSupplier(context: ConfigurableApplicationContext): ServiceInstanceListSupplier {
+	return ServiceInstanceListSupplier.builder()
+		.withDiscoveryClient()
+		.withHealthChecks()
+		.build(context);
 }
