5be29dfc27687e80ce07e8f2e5cf15783f6f8b14
d najd <dimitar.hristijan.najdovski@gmail.com>
2023-02-07 22:41:34 +0100

    backup

---
+++ test-auth-service5/src/main/kotlin/com/cythero/cityguide/testauthservice5/TestAuthService5Application.kt
@@ -0,0 +1,11 @@
+package com.cythero.cityguide.testauthservice5
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestAuthService5Application
+
+fun main(args: Array<String>) {
+	runApplication<TestAuthService5Application>(*args)
+}
--- users-service/src/main/kotlin/com/cythero/cityguide/usersservice/config/SecurityConfiguration.kt
+++ users-service/src/main/kotlin/com/cythero/cityguide/usersservice/config/SecurityConfiguration.kt
@@ -1,18 +1,25 @@
 package com.cythero.cityguide.usersservice.config
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
-import org.springframework.security.config.web.server.ServerHttpSecurity
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
-import org.springframework.security.core.userdetails.User
-import org.springframework.security.crypto.factory.PasswordEncoderFactories
-import org.springframework.security.crypto.password.NoOpPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.SecurityFilterChain
 
+
+
+
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfiguration : WebSecurityConfigurerAdapter() {
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic()
+    }
+}
+
+/*
 @EnableWebFluxSecurity
 class SecurityConfig{
 
@@ -53,3 +60,5 @@ class SecurityConfig{
     }
 
 }
+
+ */
